f450fbfb3b44e34d9aafba27657143906585bb9c
Lynden Jay Evans <gameshopcorp@gmail.com>
2024-07-12 04:03:52 -0400

    Making GUI Using NiftyGUI

--- app/src/main/java/com/gameshopcorp/gameshop/Main.java
+++ app/src/main/java/com/gameshopcorp/gameshop/Main.java
@@ -3,11 +3,14 @@ package com.gameshopcorp.gameshop;
 import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.niftygui.NiftyJmeDisplay;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
 import com.jme3.texture.Texture;
 
 import java.util.logging.Logger;
+
+import de.lessvoid.nifty.Nifty;
 
 /**
  *
@@ -30,5 +33,27 @@ public class Main extends SimpleApplication {
 
         geom.setMaterial(mat);
         rootNode.attachChild(geom);
+
+//        Screen screen = nifty.getCurrentScreen();
+//        Element layer = screen.findElementByName("baseLayer");
+//
+//        // create a 8px height red panel
+//        PanelCreator createPanel = new PanelCreator();
+//        createPanel.setHeight("8px");
+//        createPanel.setBackgroundColor("#f00f");
+//        Element newPanel = createPanel.create(nifty, screen, layer);
+
+
+        MyStartScreen myStartScreen = new MyStartScreen();
+        myStartScreen.initialize(this);
+        NiftyJmeDisplay niftyDisplay = NiftyJmeDisplay.newNiftyJmeDisplay(
+                assetManager, inputManager, audioRenderer, guiViewPort);
+/** Create a new NiftyGUI object */
+        Nifty nifty = niftyDisplay.getNifty();
+/** Read your XML and initialize your custom ScreenController */
+        nifty.fromXml("Interface/screen.xml", "GScreen0", myStartScreen);
+// nifty.fromXml("Interface/helloworld.xml", "start", new MySettingsScreen(data));
+// attach the Nifty display to the gui view port as a processor
+        guiViewPort.addProcessor(niftyDisplay);
     }
 }
